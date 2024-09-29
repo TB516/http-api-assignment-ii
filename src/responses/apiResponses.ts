5b672c0f4430d76233c46d4be229ebf1bb95d806
TB516 <tberrios9800@gmail.com>
2024-09-29 02:33:52 -0400

    Completed server api responses

--- src/responses/apiResponses.ts
+++ src/responses/apiResponses.ts
@@ -1,4 +1,115 @@
 import { IncomingMessage, ServerResponse } from 'http';
+import { User } from './User';
+import { ErrorMessage } from './ErrorMessage';
+
+const users: User[] = [];
 
 const getUsers = (request: IncomingMessage, response: ServerResponse) => {
+  const json = JSON.stringify(users);
+
+  response.writeHead(200, 'Success', {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(json, 'utf8'),
+  });
+
+  if (request.method !== 'HEAD') { response.write(json); }
+
+  return response.end();
+};
+
+const notFound = (
+  request: IncomingMessage,
+  response: ServerResponse,
+  errMessage?: ErrorMessage,
+) => {
+  const message = JSON.stringify(errMessage);
+
+  response.writeHead(404, 'Not Found', {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(message, 'utf8'),
+  });
+
+  if (request.method !== 'HEAD') { response.write(message); }
+
+  return response.end();
+};
+
+// eslint-disable-next-line arrow-body-style
+const pageNotFound = (request: IncomingMessage, response: ServerResponse) => {
+  return notFound(request, response, { id: 'notFound', message: 'The page you are looking for was not found.' } as ErrorMessage);
+};
+
+const badRequest = (
+  request: IncomingMessage,
+  response: ServerResponse,
+  errMessage: ErrorMessage,
+) => {
+  const message = JSON.stringify(errMessage);
+
+  response.writeHead(400, 'Bad Request', {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(message, 'utf8'),
+  });
+
+  if (request.method !== 'HEAD') { response.write(message); }
+
+  return response.end();
+};
+
+const updateUser = (
+  request: IncomingMessage,
+  response: ServerResponse,
+  updateIndex: number,
+  incomingUser: User,
+) => {
+  const user = users[updateIndex];
+
+  user.age = incomingUser.age;
+  user.times!.lastUpdated = Date.now();
+
+  response.writeHead(204, 'Updated');
+  return response.end();
+};
+
+const addUser = (request: IncomingMessage, response: ServerResponse) => {
+  // Using query params because getting a body out of an incoming message is giving me a stroke
+  const params = new URL(`https://${request.headers.host}${request.url!}`).searchParams;
+  const incomingUser = { name: params.get('name'), age: Number.parseInt(params.get('age')!, 10) } as User;
+
+  if (incomingUser.name === '' || Number.isNaN(incomingUser.age)) {
+    return badRequest(request, response, {
+      id: 'addUserMissingParams',
+      message: 'Name and age are both required.',
+    } as ErrorMessage);
+  }
+
+  const curIndex = users.findIndex((user) => user.name === incomingUser.name);
+
+  if (curIndex !== -1) {
+    return updateUser(request, response, curIndex, incomingUser);
+  }
+
+  incomingUser.times = {
+    created: Date.now(),
+    lastUpdated: Date.now(),
+  };
+  users.push(incomingUser);
+
+  const message = JSON.stringify({ message: 'Created Successfully' });
+
+  response.writeHead(201, 'Created', {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(message, 'utf8'),
+  });
+
+  response.write(message);
+
+  return response.end();
+};
+
+export {
+  getUsers,
+  notFound,
+  pageNotFound,
+  addUser,
 };
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import http, { IncomingMessage, ServerResponse } from 'http';
 import { getIndexCss, getIndexHtml } from './responses/htmlResponses';
+import {
+  addUser, getUsers, pageNotFound,
+} from './responses/apiResponses';
 
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-unused-vars, max-len
 type ResponseMethod = (arg1: IncomingMessage, arg2: ServerResponse) => void;
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const routes: Record<string, ResponseMethod> = {
   '/': getIndexHtml,
   '/style.css': getIndexCss,
-  404: () => { },
+  '/getUsers': getUsers,
+  '/addUser': addUser,
+  '/notReal': pageNotFound,
 };
 
 const onRequest = (request: IncomingMessage, response: ServerResponse) => {
@@ -18,7 +23,7 @@ const onRequest = (request: IncomingMessage, response: ServerResponse) => {
     return routes[parsedUrl.pathname](request, response);
   }
 
-  return routes[404](request, response);
+  return routes['/notReal'](request, response);
 };
 
 http.createServer(onRequest).listen(port, () => {
